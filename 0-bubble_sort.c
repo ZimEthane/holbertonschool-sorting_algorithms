50204ca5088ce4938ea3ac1874382fea8dec9a20
Ethane Zimmermann <zepsydu82@hotmail.com>
2025-12-10 13:24:26 +0000

    feat : create all file for begin the projet

---
+++ 0-bubble_sort.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - Sorts an array of integers in ascending order
+ *                 using the Bubble sort algorithm
+ *
+ * @array: The array to be sorted
+ * @size: Number of elements in @array
+ */
+void bubble_sort(int *array, size_t size)
+{
+
+}
