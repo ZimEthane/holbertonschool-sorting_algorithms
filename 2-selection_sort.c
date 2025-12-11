1874898dc90d528fa247992e9ab3a60503b76f56
Ethane Zimmermann <zepsydu82@hotmail.com>
2025-12-11 10:18:12 +0000

    fiw : betty code style

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,10 +1,10 @@
 #include "sort.h"
 
 /**
- * selection_sort - sorts an array of integers in ascending order
- *                  using the Selection sort algorithm
- * @array: array of integers
- * @size: size of the array
+ * selection_sort - Sorts an array of integers in ascending order
+ *                   using the Selection sort algorithm.
+ * @array: Pointer to the array of integers to be sorted.
+ * @size: Number of elements in the array.
  */
 void selection_sort(int *array, size_t size)
 {
