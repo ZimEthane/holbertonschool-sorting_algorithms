0a74be6da4838b32283fbdb5c94297eefe5d4559
Nyde2482 <Callejoesteban@gmail.com>
2025-12-11 10:47:47 +0000

    fix : corriger la déclaration de swap_value et ajouter la logique de tri

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,5 +8,22 @@
  */
 void bubble_sort(int *array, size_t size)
 {
+	size_t i = 0;
+	size_t j = 0;
 
+	size_t swap_value = 0
+
+	if (array == NULL || size < 2) 
+		return;
+
+	for (i = 0; i < size - 1; i++) 
+		for (j = 0; j < size - 1; j++)
+			if (array[j] > array[j + 1])
+			{
+				swap_value = array[j + 1];
+				array[j + 1] = array[j];
+				array[j] = swap_value;
+
+				print_array(array, size);
+			}
 }
